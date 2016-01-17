42b380edb4d3088c4aa8d3e3998e27b3bb7496b4
wSedlacek <wcsedlacek0@gmail.com>
2016-01-16 18:24:34 -0600

    Hammerhead bring up [1/3]
    Based on vanir device trees
    
    Change-Id: Ia70edd2ab4b3e2a535a7c888bfe00e92938978c4

---
+++ exodus.mk
@@ -0,0 +1,25 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOTANIMATION_HALF_RES := true
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/exodus/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/exodus/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hammerhead
+PRODUCT_NAME := exodus_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hammerhead \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29S/2489379:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29S 2489379 release-keys"
